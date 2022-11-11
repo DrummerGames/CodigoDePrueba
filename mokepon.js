c0ab20ebc9a6bb4d62c8c08d6fa18ff4d4bf3153
DrummerGames <98218884+DrummerGames@users.noreply.github.com>
2022-11-10 23:36:50 -0400

    Update mokepon.js

--- mokepon.js
+++ mokepon.js
@@ -6,24 +6,43 @@ let vidasJugador = 3
 let vidasOponente = 3
 let veredictoFinal
 
+
 function iniciarJuego() {
+   
+   let seccionSeleccionarAtaque = document.getElementById ('seleccionar-ataque')
+   seccionSeleccionarAtaque.style.display = 'none'
+
+   let seccionReiniciar = document.getElementById ('reiniciar')
+   seccionReiniciar.style.display = 'none'
+   
     let botonPersonajeJugador = document.getElementById('boton-personaje')
     let botonFuego = document.getElementById ('boton-fuego')
     let botonAgua = document.getElementById ('boton-agua')
     let botonTierra = document.getElementById ('boton-tierra')
+    let botonReiniciar = document.getElementById ("boton-reiniciar")
+    
 
     botonPersonajeJugador.addEventListener('click', seleccionarPersonajeJugador)
     botonFuego.addEventListener('click', ataqueFuego)
     botonAgua.addEventListener('click', ataqueAgua)
     botonTierra.addEventListener('click', ataqueTierra)
+    botonReiniciar.addEventListener('click', reiniciarJuego)
+
 }
 
+
 function seleccionarPersonajeJugador() {
+    let seccionSeleccionarPersonaje = document.getElementById ('seleccionar-personaje')
+    seccionSeleccionarPersonaje.style.display = 'none'
+    
+    let seccionSeleccionarAtaque = document.getElementById ('seleccionar-ataque')
+    seccionSeleccionarAtaque.style.display = 'block'
+   
     let inputsubzero = document.getElementById('subzero').checked
     let inputscorpion = document.getElementById('scorpion').checked
     let inputtremor = document.getElementById('tremor').checked
     let spanPersonajeJugador = document.getElementById ("personaje-jugador")
-   
+
     if (inputsubzero == true){
         spanPersonajeJugador.innerHTML = "Subzero"
     } else if (inputscorpion == true) {
@@ -54,22 +73,25 @@ function seleccionarPersonajeJugador() {
 function ataqueFuego () {
     ataqueJugador = 'Fuego'
     ataqueAleatorioOponente()
-    crearMensaje() 
+    crearMensaje()
+    crearMensajeFinal() 
     
 }
 function ataqueAgua () {
     ataqueJugador = 'Agua'
     ataqueAleatorioOponente()
-    crearMensaje() 
+    crearMensaje()
+    crearMensajeFinal() 
 }
 function ataqueTierra () {
     ataqueJugador = 'Tierra'
     ataqueAleatorioOponente()
-    crearMensaje() 
+    crearMensaje()
+    crearMensajeFinal() 
 }
-    
-function ataqueAleatorioOponente() {
- ataqueOponente =  aleatorio(1,3)
+
+function ataqueAleatorioOponente() {   
+  ataqueOponente =  aleatorio(1,3)
 
     if (ataqueOponente == 1){
         ataqueOponente = 'Fuego'
@@ -81,7 +103,7 @@ function ataqueAleatorioOponente() {
     
     combate()
     
-}
+ }
 
 
 
@@ -95,7 +117,7 @@ function combate (){
         
         if (ataqueJugador == ataqueOponente ){
         resultadoFinal=("Empate 🤔")    
-      
+ 
     } 
         else if (ataqueJugador == "Tierra" && ataqueOponente == "Fuego"){
         resultadoFinal = ("Has ganado 🎉")    
@@ -116,22 +138,21 @@ function combate (){
         vidasJugador = vidasJugador -1
         spanvidasJugador.innerHTML = vidasJugador 
        
-
     }
        
-        veredicto()
+         veredicto()
    
 }
    
     function veredicto (){
-    
-    
+   
     if (vidasOponente == 0){
         veredictoFinal = ("Has ganado la batalla !FELICITACIONES")
     } else if (vidasJugador == 0){
-        veredictoFinal=("has perdido la batalla, retirada")
+        veredictoFinal= ("HAS PERDIDO LA BATALLA, RETIRADA 🏃‍♂️")
     }
-
+    
+    
 }
 
    
@@ -140,25 +161,63 @@ function combate (){
 	    let parrafo = document.createElement("p")
 	    parrafo.innerHTML = "Tu personaje ataco con " + ataqueJugador + " , el contrincante ataco con " + ataqueOponente + ". "  + resultadoFinal 
 	    SectionMensajes.appendChild (parrafo) //el APENDCHILD sirve para agregar algo creado en JS  dentro del HTMl
-
+       
 }
     
-    function crearMensajeFinal (veredictoFinal){
-    let SectionMensajes = document.getElementById("mensajes") 
-    let parrafo = document.createElement("p")
-    parrafo.innerHTML = veredictoFinal
-    SectionMensajes.appendChild (parrafo)
+    
 
-}     
+     function crearMensajeFinal (){  
+        
+            let SectionMensajes = document.getElementById("mensajes") 
+            let parrafo = document.createElement("p")
+            if (vidasOponente <= 0 )
+          
+            {   parrafo.innerHTML = veredictoFinal
+                SectionMensajes.appendChild (parrafo) 
+            
+                let botonFuego = document.getElementById ('boton-fuego')
+                let botonAgua = document.getElementById ('boton-agua')
+                let botonTierra = document.getElementById ('boton-tierra')
+            
+                botonFuego.disabled=true
+                botonAgua.disabled=true
+                botonTierra. disabled=true
 
+                let seccionReiniciar = document.getElementById ('reiniciar')
+                seccionReiniciar.style.display = 'block'
+            
+            } 
+                else if (vidasJugador <= 0)
+          
+            {   parrafo.innerHTML = veredictoFinal
+                SectionMensajes.appendChild (parrafo)
+            
+            
+                let botonFuego = document.getElementById ('boton-fuego')
+                let botonAgua = document.getElementById ('boton-agua')
+                let botonTierra = document.getElementById ('boton-tierra')
+            
+                botonFuego.disabled=true
+                botonAgua.disabled=true
+                botonTierra. disabled=true
 
+                let seccionReiniciar = document.getElementById ('reiniciar')
+                seccionReiniciar.style.display = 'block'
+            
+            }
 
-
+        }  
+    
+       
+       
+    function reiniciarJuego() {
+       location.reload()
+    }
+    
     function aleatorio(min, max){
     return Math.floor(Math.random () * (max - min + 1) + min)
-}
+    }
 
 
 window.addEventListener('load', iniciarJuego)
-
 
